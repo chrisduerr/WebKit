172179592d8cc0e4efec364ad9205b911a90f96a
Christian Duerr <contact@christianduerr.com>
2025-03-29 20:52:59 +0100

    Add touch context menu support
    This patch uses the new WPE Platform's gesture controller to send
    synthetic right-click events whenever holding a touch point in position
    for a second. The synthetic right-click event will then automatically
    generate a context menu event for interested clients.

--- Source/WebKit/UIProcess/API/wpe/WPEWebViewPlatform.h
+++ Source/WebKit/UIProcess/API/wpe/WPEWebViewPlatform.h
@@ -90,6 +90,10 @@ private:
 
     gboolean handleEvent(WPEEvent*);
     void handleGesture(WPEEvent*);
+    void simulateClick(double x, double y, WPEModifiers modifiers, guint button);
+#if ENABLE(CONTEXT_MENUS)
+    void handleLongPress();
+#endif
 
     GRefPtr<WPEView> m_wpeView;
     RefPtr<WebKit::AcceleratedBackingStoreDMABuf> m_backingStore;
@@ -100,6 +104,12 @@ private:
 #if ENABLE(FULLSCREEN_API)
     bool m_viewWasAlreadyInFullScreen { false };
 #endif
+#if ENABLE(CONTEXT_MENUS)
+    RunLoop::Timer m_longPressTimeout;
+    bool m_longPressFired;
+    double m_longPressX;
+    double m_longPressY;
+#endif // ENABLE(CONTEXT_MENUS)
 };
 
 } // namespace WKWPE
