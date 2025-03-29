f3c8be7de16a26ea0ada8b5b0207cd8f199de0cd
Christian Duerr <contact@christianduerr.com>
2025-03-29 04:25:49 +0100

    Add WPE favicon support
    This adds favicon support to the WPE backend, based on the existing code
    introduced by GTK.
    
    A new `WebKitFavicon` type is introduced as the return value for WPE's
    favicon calls, which exposes the favicon size and its content through
    separate functions.
    
    The favicon data itself is unmodified and in BGRA format.

--- Source/WebKit/UIProcess/API/glib/WebKitWebsiteDataManager.cpp
+++ Source/WebKit/UIProcess/API/glib/WebKitWebsiteDataManager.cpp
@@ -39,7 +39,7 @@
 #include <wtf/glib/GUniquePtr.h>
 #include <wtf/glib/WTFGType.h>
 
-#if PLATFORM(GTK) && ENABLE(2022_GLIB_API)
+#if ENABLE(2022_GLIB_API)
 #include "WebKitFaviconDatabasePrivate.h"
 #endif
 
@@ -95,7 +95,7 @@ struct _WebKitWebsiteDataManagerPrivate {
     CString baseDataDirectory;
     CString baseCacheDirectory;
 
-#if PLATFORM(GTK) && ENABLE(2022_GLIB_API)
+#if ENABLE(2022_GLIB_API)
     GRefPtr<WebKitFaviconDatabase> faviconDatabase;
 #endif
 
@@ -1085,7 +1085,7 @@ void webkit_website_data_manager_set_network_proxy_settings(WebKitWebsiteDataMan
 }
 #endif
 
-#if PLATFORM(GTK) && ENABLE(2022_GLIB_API)
+#if ENABLE(2022_GLIB_API)
 static String webkitWebsiteDataManagerGetFaviconDatabasePath(WebKitWebsiteDataManager* manager)
 {
     if (webkit_website_data_manager_is_ephemeral(manager))
